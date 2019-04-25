eedd9c552027aafccd1daa8da370758eb2d69897
Kevin Schroeder <schroederkevin94@gmail.com>
2019-04-25 08:12:24 -0400

    All functionality is working and README is updated

--- README.md
+++ README.md
@@ -1,1 +1,89 @@
 # liri-node-app
+
+LIRI is a Language Interpretation and Recognition Interface.
+Use LIRI to get information about the song or movie of your
+choice and it can get an artist's upcoming concert information
+
+## Installs
+
+The [package.json](https://github.com/Meggin/liri-node-app/blob/master/package.json)
+lists dependent node packages, but for your convenvice, these are the ones to install.
+
+### Node Spotify API
+
+`npm install --save node-spotify-api`
+
+### Moment
+
+`npm install mement`
+
+### FS
+
+`npm install fs`
+
+### Axios
+
+`npm install axios`
+
+## Get Started
+
+Here's a quick rundom of the commands you can use in LIRI.
+
+### Get Concert Info
+
+This will search the Bands in Town Artist Events API (`"https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"`) for an artist and render the following information about each event to the terminal:
+
+     * Name of the venue
+
+     * Venue location
+
+     * Date of the Event (use moment to format this as "MM/DD/YYYY")
+
+**You must replace the Bands in Town API key on line 99**
+
+`node liri.js concert-this`
+
+### Get Song Info
+
+This will show the following information about the song in your terminal/bash window
+
+     * Artist(s)
+
+     * The song's name
+
+     * A preview link of the song from Spotify
+
+     * The album that the song is from
+
+   * If no song is provided then your program will default to "The Sign" by Ace of Base.
+
+   **You must replace the SPOTIFY_ID and SPOTIFY_SECRET in the .env file**
+
+`node liri.js spotify-this-song "All The Small Things"`
+
+### Get Movie Info
+
+This will output the following information to your terminal/bash window:
+
+     ```
+       * Title of the movie.
+       * Year the movie came out.
+       * IMDB Rating of the movie.
+       * Rotten Tomatoes Rating of the movie.
+       * Country where the movie was produced.
+       * Language of the movie.
+       * Plot of the movie.
+       * Actors in the movie.
+     ```
+
+   * If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
+
+   **You must replace the OMDB API key on line 124**
+
+`node liri.js movie-this "Star Wars"`
+
+### Get Command from Random.txt
+
+Gets text inside a the radnom.txt and does what it says:
+
+`node liri.js do-what-it-says`
--- liri.js
+++ liri.js
@@ -1,66 +1,99 @@
 // define global letiables
 require("dotenv").config();
 let keys = require("./keys");
-let inquirer = require("inquirer");
+//let inquirer = require("inquirer");
 let Spotify = require("node-spotify-api");
 let moment = require('moment')
 let axios = require('axios');
+let fs = require('fs');
+
+
 let spotify = new Spotify(keys.spotify);
+
+let song = '';
+let artist = '';
+let movie = '';
+
 let command = process.argv;
-
-
-function getSong() {
-    inquirer.prompt([{
-        type: "input",
-        message: "Which song whould you like me to look up?",
-        name: "query"
-    }]).then(function (inquirerResponse) {
-        if (inquirerResponse.query !== '') {
-            spotify.search({
-                type: 'track',
-                query: inquirerResponse.query,
-                limit: 1
-            }).then(function (response) {
-                //console.log(response);
-                let songObj = response.tracks.items[0];
-                let artistName = songObj.album.artists[0].name;
-                //console.log(songObj);
-                console.log(`
-                            Artist: ${artistName}
-                            Song title: ${inquirerResponse.query}
-                            Album Name: ${songObj.album.name}
-                            Preview URL: ${songObj.preview_url}
-                            `);
-            });
-        } else {
-            spotify.search({
-                type: 'track',
-                query: 'The Sign Ace of Base',
-                limit: 1
-            }).then(function (response) {
-                //console.log(response);
-                let songObj = response.tracks.items[0];
-                let artistName = songObj.album.artists[0].name;
-                //console.log(songObj);
-                console.log(`
-                            Artist: ${artistName}
-                            Song title: 'The Sign'
-                            Album Name: ${songObj.album.name}
-                            Preview URL: ${songObj.preview_url}
-                            `);
-            });
-        }
+String.prototype.toProperCase = function () {
+    return this.replace(/\w\S*/g, function (txt) {
+        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
     });
 };
 
+if (command[2] === 'concert-this') {
+    artist = command[3];
+    getConcert();
+} else if (command[2] === 'spotify-this-song') {
+    song = command[3];
+    getSong();
+} else if (command[2] === 'movie-this') {
+    movie = command[3];
+    getMovie();
+} else if (command[2] === 'do-what-it-says') {
+    fs.readFile('random.txt', 'utf8', function (err, data) {
+        if (err) {
+            console.log(err);
+        } else {
+            let dataArr = data.split(',');
+            if (dataArr[0] === 'concert-this') {
+                artist = dataArr[1];
+                getConcert();
+            } else if (dataArr[0] === 'spotify-this-song') {
+                song = dataArr[1];
+                getSong();
+            } else if (dataArr[0] === 'movie-this') {
+                movie = dataArr[1];
+                getMovie();
+            }
+        }
+
+    });
+}
+
+function getSong() {
+    //console.log(command);
+    if (song !== undefined) {
+        spotify.search({
+            type: 'track',
+            query: song,
+            limit: 1
+        }).then(function (response) {
+            //console.log(response);
+            let songObj = response.tracks.items[0];
+            let artistName = songObj.album.artists[0].name;
+            let displaySong = song.toProperCase();
+            //console.log(songObj);
+            console.log(`
+            Artist: ${artistName}
+            Song title: ${displaySong}
+            Album Name: ${songObj.album.name}
+            Preview URL: ${songObj.preview_url}
+            `);
+        });
+
+    } else {
+        spotify.search({
+            type: 'track',
+            query: 'The Sign Ace of Base',
+            limit: 1
+        }).then(function (response) {
+            //console.log(response);
+            let songObj = response.tracks.items[0];
+            let artistName = songObj.album.artists[0].name;
+            //console.log(songObj);
+            console.log(`
+            Artist: ${artistName}
+            Song title: 'The Sign'
+            Album Name: ${songObj.album.name}
+            Preview URL: ${songObj.preview_url}
+            `);
+        });
+    }
+}
 
 function getConcert() {
-    inquirer.prompt([{
-        type: "input",
-        message: "What artist do you want to search?",
-        name: "query"
-    }]).then(function (inquirerResponse) {
-        let artist = inquirerResponse.query;
+    if (artist !== undefined) {
         axios({
             method: 'get',
             url: `https://rest.bandsintown.com/artists/${artist}/events?app_id=codingbootcamp`,
@@ -68,81 +101,201 @@ function getConcert() {
         }).then(function (response) {
             let showArr = response.data;
             for (let i = 0; i < showArr.length; i++) {
-                // let venueName = showArr[i].venue.name;
-                // let venueCity = showArr[i].venue.city;
-                // let venueCountry = showArr[i].venue.country;
                 let orgShowDate = showArr[i].datetime;
                 let showDate = moment(orgShowDate).format('L');
 
                 console.log(`
-                        Venue Name: ${showArr[i].venue.name}
-                        Venue Location: ${showArr[i].venue.city}, ${showArr[i].venue.country}
-                        Show Date: Venue Location: ${showDate}
-                        `);
+                Venue Name: ${showArr[i].venue.name}
+                Venue Location: ${showArr[i].venue.city}, ${showArr[i].venue.country}
+                Show Date: Venue Location: ${showDate}
+                `);
             };
-        }).catch(function (err) {
-            console.log("The artists name is not defined. Please try a different artist");
         });
-    });
+    } else {
+        console.log("The artist name is not defined. Please try a different artist (remeber to use quotes around the artist's name)");
+    };
+
 }
 
 function getMovie() {
-    inquirer.prompt([{
-        type: "input",
-        message: "Which movie whould you like me to look up?",
-        name: "query"
-    }]).then(function (inquirerResponse) {
-        if (inquirerResponse.query !== '') {
-            let movie = inquirerResponse.query;
-            axios({
-                method: 'get',
-                url: `http://www.omdbapi.com/?apikey=c3f10979&t=${movie}`,
-                responseType: 'json'
-            }).then(function (response) {
-                console.log(response.data);
-                let releaseDate = response.data.Released;
-                let releaseYear = moment(releaseDate).format('YYYY');
-                console.log(`
-                Title: ${response.data.Title}
-                Year Released: ${releaseYear}
-                IMDB Rating: ${response.data.Ratings[0].Value}
-                RottenTomatoes Rating: ${response.data.Ratings[1].Value}
-                Country: ${response.data.Country}
-                Language: ${response.data.Language}
-                Plot: ${response.data.Plot}
-                Actors: ${response.data.Actors}
-                `)
-            });
-        } else {
-            let movie = 'Mr. Nobody';
-            axios({
-                method: 'get',
-                url: `http://www.omdbapi.com/?apikey=c3f10979&t=${movie}`,
-                responseType: 'json'
-            }).then(function (response) {
-                console.log(response.data);
-                let releaseDate = response.data.Released;
-                let releaseYear = moment(releaseDate).format('YYYY');
-                console.log(`
-                Title: ${response.data.Title}
-                Released: ${releaseYear}
-                IMDB Rating: ${response.data.Ratings[0].Value}
-                RottenTomatoes Rating: ${response.data.Ratings[1].Value}
-                Country: ${response.data.Country}
-                Language: ${response.data.Language}
-                Plot: ${response.data.Plot}
-                Actors: ${response.data.Actors}
-                `)
-            });
-        }
-    });
-};
+    if ( movie !== '') {
+        axios({
+            method: 'get',
+            url: `http://www.omdbapi.com/?apikey=c3f10979&t=${movie}`,
+            responseType: 'json'
+        }).then(function (response) {
+            //console.log(response.data);
+            let releaseDate = response.data.Released;
+            let releaseYear = moment(releaseDate).format('YYYY');
+            console.log(`
+            Title: ${response.data.Title}
+            Year Released: ${releaseYear}
+            IMDB Rating: ${response.data.Ratings[0].Value}
+            RottenTomatoes Rating: ${response.data.Ratings[1].Value}
+            Country: ${response.data.Country}
+            Language: ${response.data.Language}
+            Plot: ${response.data.Plot}
+            Actors: ${response.data.Actors}
+            `)
+        });
+    } else {
+        let movie = 'Mr. Nobody';
+        axios({
+            method: 'get',
+            url: `http://www.omdbapi.com/?apikey=c3f10979&t=${movie}`,
+            responseType: 'json'
+        }).then(function (response) {
+            //console.log(response.data);
+            let releaseDate = response.data.Released;
+            let releaseYear = moment(releaseDate).format('YYYY');
+            console.log(`
+            Title: ${response.data.Title}
+            Released: ${releaseYear}
+            IMDB Rating: ${response.data.Ratings[0].Value}
+            RottenTomatoes Rating: ${response.data.Ratings[1].Value}
+            Country: ${response.data.Country}
+            Language: ${response.data.Language}
+            Plot: ${response.data.Plot}
+            Actors: ${response.data.Actors}
+            `)
+        });
+    }
 
-//getsong();
-if (command[2] === 'concert-this') {
-    getConcert();
-} else if (command[2] === 'spotify-this-song') {
-    getSong();
-} else if (command[2] === 'movie-this') {
-    getMovie();
 }
+
+
+
+
+
+
+
+// function getSong() {
+//     inquirer.prompt([{
+//         type: "input",
+//         message: "Which song whould you like me to look up?",
+//         name: "query"
+//     }]).then(function (inquirerResponse) {
+//         if (inquirerResponse.query !== '') {
+//             spotify.search({
+//                 type: 'track',
+//                 query: inquirerResponse.query,
+//                 limit: 1
+//             }).then(function (response) {
+//                 //console.log(response);
+//                 let songObj = response.tracks.items[0];
+//                 let artistName = songObj.album.artists[0].name;
+//                 //console.log(songObj);
+//                 console.log(`
+//                             Artist: ${artistName}
+//                             Song title: ${inquirerResponse.query}
+//                             Album Name: ${songObj.album.name}
+//                             Preview URL: ${songObj.preview_url}
+//                             `);
+//             });
+//         } else {
+//             spotify.search({
+//                 type: 'track',
+//                 query: 'The Sign Ace of Base',
+//                 limit: 1
+//             }).then(function (response) {
+//                 //console.log(response);
+//                 let songObj = response.tracks.items[0];
+//                 let artistName = songObj.album.artists[0].name;
+//                 //console.log(songObj);
+//                 console.log(`
+//                             Artist: ${artistName}
+//                             Song title: 'The Sign'
+//                             Album Name: ${songObj.album.name}
+//                             Preview URL: ${songObj.preview_url}
+//                             `);
+//             });
+//         }
+//     });
+// };
+
+
+// function getConcert() {
+//     inquirer.prompt([{
+//         type: "input",
+//         message: "What artist do you want to search?",
+//         name: "query"
+//     }]).then(function (inquirerResponse) {
+//         let artist = inquirerResponse.query;
+//         axios({
+//             method: 'get',
+//             url: `https://rest.bandsintown.com/artists/${artist}/events?app_id=codingbootcamp`,
+//             responseType: 'json'
+//         }).then(function (response) {
+//             let showArr = response.data;
+//             for (let i = 0; i < showArr.length; i++) {
+//                 // let venueName = showArr[i].venue.name;
+//                 // let venueCity = showArr[i].venue.city;
+//                 // let venueCountry = showArr[i].venue.country;
+//                 let orgShowDate = showArr[i].datetime;
+//                 let showDate = moment(orgShowDate).format('L');
+
+//                 console.log(`
+//                         Venue Name: ${showArr[i].venue.name}
+//                         Venue Location: ${showArr[i].venue.city}, ${showArr[i].venue.country}
+//                         Show Date: Venue Location: ${showDate}
+//                         `);
+//             };
+//         }).catch(function (err) {
+//             console.log("The artists name is not defined. Please try a different artist");
+//         });
+//     });
+// }
+
+// function getMovie() {
+//     inquirer.prompt([{
+//         type: "input",
+//         message: "Which movie whould you like me to look up?",
+//         name: "query"
+//     }]).then(function (inquirerResponse) {
+//         if (inquirerResponse.query !== '') {
+//             let movie = inquirerResponse.query;
+//             axios({
+//                 method: 'get',
+//                 url: `http://www.omdbapi.com/?apikey=c3f10979&t=${movie}`,
+//                 responseType: 'json'
+//             }).then(function (response) {
+//                 console.log(response.data);
+//                 let releaseDate = response.data.Released;
+//                 let releaseYear = moment(releaseDate).format('YYYY');
+//                 console.log(`
+//                 Title: ${response.data.Title}
+//                 Year Released: ${releaseYear}
+//                 IMDB Rating: ${response.data.Ratings[0].Value}
+//                 RottenTomatoes Rating: ${response.data.Ratings[1].Value}
+//                 Country: ${response.data.Country}
+//                 Language: ${response.data.Language}
+//                 Plot: ${response.data.Plot}
+//                 Actors: ${response.data.Actors}
+//                 `)
+//             });
+//         } else {
+//             let movie = 'Mr. Nobody';
+//             axios({
+//                 method: 'get',
+//                 url: `http://www.omdbapi.com/?apikey=c3f10979&t=${movie}`,
+//                 responseType: 'json'
+//             }).then(function (response) {
+//                 console.log(response.data);
+//                 let releaseDate = response.data.Released;
+//                 let releaseYear = moment(releaseDate).format('YYYY');
+//                 console.log(`
+//                 Title: ${response.data.Title}
+//                 Released: ${releaseYear}
+//                 IMDB Rating: ${response.data.Ratings[0].Value}
+//                 RottenTomatoes Rating: ${response.data.Ratings[1].Value}
+//                 Country: ${response.data.Country}
+//                 Language: ${response.data.Language}
+//                 Plot: ${response.data.Plot}
+//                 Actors: ${response.data.Actors}
+//                 `)
+//             });
+//         }
+//     });
+// };
+
+
